a4d029675b02c48e2f8434caafc5bf8e123eeb60
yogesh-hackx <yogesh.hackx@gmail.com>
2019-07-31 17:23:25 +0530

    added support for speech

--- listenAndDecode.py
+++ listenAndDecode.py
@@ -1,8 +1,10 @@
-import os
+import os, pyttsx3
 
 rec = "sox -t alsa default recording.wav silence 1 0.1 2% 1 0.5 2%"
 
 decode = "multimon-ng -t wav -a DTMF recording.wav > out.txt"
+
+engine = pyttsx3.init()
 
 while True:
     #listen for audio
@@ -17,5 +19,7 @@ while True:
 
     try:
         print((results[results.index("DTMF:") + 1]).center(50, "*"))        #For debugging
+        engine.say(int(results[results.index("DTMF:") + 1]))    #speaks inputted digit
+        engine.runAndWait()
     except:
         pass
